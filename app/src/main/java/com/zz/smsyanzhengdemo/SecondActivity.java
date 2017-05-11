f3be19796ac6e1366f651dfa03033f1ca930cf2e
leizhong <675114706@qq.com>
2017-05-11 15:07:06 +0800

    first commit

---
+++ app/src/main/java/com/zz/smsyanzhengdemo/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.zz.smsyanzhengdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 16-11-23.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
